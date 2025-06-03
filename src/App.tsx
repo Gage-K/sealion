b41131f092f0009285374de151c36bad6d90548b
Gage-K <krausegage@gmail.com>
2025-06-03 10:21:55 -0400

    Init drum synthesis + Creates initial drum kit + Implements logic in repeat function to handle different drum synths + Fixes memory leak bug with creating a clock

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,55 @@ import * as Tone from "tone";
 
 // lib
 import { type Sequence } from "./types/types";
-import { getTracksByScale } from "./utils/utils";
+import { getTrackOfNote, getTracksByScale } from "./utils/utils";
 import { dotStyles } from "./lib/seqStyles";
 
 // components
 import Metronome from "./components/Metronome";
 
+const CURRENT_MODE: "synth" | "drum" = "drum";
+
 const cPentatonic: Sequence = getTracksByScale("C", 4, "pentatonic");
 
-const DEFAULT_TRACK_SET: Sequence = cPentatonic; // specifies base octave
+type DrumKit = [
+  kick: Tone.MembraneSynth,
+  snare: Tone.NoiseSynth,
+  hihat: Tone.MetalSynth
+];
 
-const synths = DEFAULT_TRACK_SET.map(() => new Tone.Synth().toDestination());
+const kick = new Tone.MembraneSynth().toDestination();
+
+const hihat = new Tone.MetalSynth({
+  envelope: { attack: 0.001, decay: 0.1, release: 0.01 },
+  harmonicity: 5.1,
+  modulationIndex: 32,
+  resonance: 4000,
+  octaves: 1.5,
+}).toDestination();
+
+const snare = new Tone.NoiseSynth({
+  noise: { type: "white" },
+  envelope: { attack: 0.001, decay: 0.2, sustain: 0 },
+}).toDestination();
+
+const drumKit: DrumKit = [kick, snare, hihat];
+
+const kitSequence: Sequence = [
+  getTrackOfNote("C", 1, "kick"),
+  getTrackOfNote("C", 4, "snare"),
+  getTrackOfNote("C", 1, "hihat"),
+];
+
+const DEFAULT_TRACK_SET: Sequence =
+  CURRENT_MODE === "synth" ? cPentatonic : kitSequence;
+
+const synths =
+  CURRENT_MODE === "drum"
+    ? drumKit
+    : DEFAULT_TRACK_SET.map(() => new Tone.Synth().toDestination());
+
+console.log(drumKit);
+console.log(synths);
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -26,43 +64,69 @@ function App() {
   const sequenceRef = useRef(sequence);
   Tone.getTransport().bpm.value = bpm;
 
+  // Handles logic for triggerign sounds on each repeat or tick of the clock
   const repeat = (time: number) => {
     setCurrentStep(beatRef.current);
 
     sequenceRef.current.forEach((track, index) => {
       const synth = synths[index];
+      console.log(synth);
       const note = track.steps[beatRef.current];
-      if (note.active) synth.triggerAttackRelease(note.note, "16n", time);
+
+      if (note.active) {
+        // Note: Different Tone.js synths require different parameters. Especially when in drum mode, there
+        // must be logic to handle the different synths used to generate the sounds.
+
+        // DRUM MODE
+        if (CURRENT_MODE === "drum") {
+          if (synth instanceof Tone.NoiseSynth) {
+            // Noise synth has specific parameters
+            synth.triggerAttackRelease("16n", time);
+          } else {
+            // Membrane and Metal synth are more similar to the standard Synth parameters
+            synth.triggerAttackRelease("C1", "16n", time);
+          }
+        } else {
+          // SYNTH MODE
+          synth.triggerAttackRelease(note.note, "16n", time);
+        }
+      }
+      // if (note.active) synth.triggerAttackRelease(note.note, "16n", time);
     });
     beatRef.current = (beatRef.current + 1) % 16;
   };
 
+  // Creates clock on mount and cleans it up on unmount to avoid memory leak
   useEffect(() => {
-    Tone.getTransport().scheduleRepeat(repeat, "16n");
+    // Instantiate clock
+    const id = Tone.getTransport().scheduleRepeat(repeat, "16n");
 
     return () => {
-      Tone.getTransport().cancel();
+      // Clean up
+      Tone.getTransport().clear(id);
     };
-  }, [isPlaying]);
+  }, []);
 
   useEffect(() => {
     sequenceRef.current = sequence;
   }, [sequence]);
 
+  // Updates playing state & current step position; Starts/stops audio
   const togglePlay = async () => {
     if (isPlaying) {
       Tone.getTransport().stop();
-      Tone.getTransport().cancel();
       Tone.getTransport().position = 0;
       setIsPlaying(false);
     } else {
       beatRef.current = 0;
       setCurrentStep(0);
+      await Tone.start();
       Tone.getTransport().start();
       setIsPlaying(true);
     }
   };
 
+  // TODO: add more checking to prevent numbers that break the BPM
   const handleBPMChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value) {
       const newBPM = parseInt(e.target.value);
@@ -72,6 +136,7 @@ function App() {
     }
   };
 
+  // Updates whether status of step of a track is active/inactive
   const updateSequence = (trackIndex: number, stepIndex: number) => {
     setSequence((prevSequence) => {
       const currentTrack = prevSequence[trackIndex];
