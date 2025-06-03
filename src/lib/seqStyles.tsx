7df7ddb972b93a9d178398a4d4c51fd2dc0019a6
Gage-K <krausegage@gmail.com>
2025-06-03 09:27:58 -0400

    Init

---
+++ src/lib/seqStyles.tsx
@@ -0,0 +1,13 @@
+export const baseDotStyle =
+  "w-4 h-4 rounded-sm border-b inset-shadow-sm ease-in-out duration-100";
+
+// TODO: rename this variable and split up to different variables
+export const dotStyles = {
+  dotInactive: `${baseDotStyle} bg-neutral-200 border-b-neutral-100`,
+  dotActive: `${baseDotStyle} bg-cyan-200 border-b-cyan-100 shadow-cyan-200/50`,
+  dotActiveStart: `${baseDotStyle} bg-orange-400 border-b-orange-300 shadow-orange-400/50`,
+  input: "bg-neutral-100 px-2 py-1 rounded-sm",
+  label: "font-medium",
+  roundButton:
+    "bg-neutral-200 p-4 rounded-3xl hover:bg-neutral-300 hover:cursor-pointer hover:shadow-sm active:bg-neutral-100",
+};
