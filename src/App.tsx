1b46777347978462131d5f0f70b4614db2580128
Gage-K <krausegage@gmail.com>
2025-06-03 15:19:45 -0400

    Add web socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import * as Tone from "tone";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import * as Tone from "tone";
 
 // lib
@@ -20,22 +20,7 @@ type DrumKit = [
   hihat: Tone.MetalSynth
 ];
 
-const kick = new Tone.MembraneSynth().toDestination();
-
-const hihat = new Tone.MetalSynth({
-  envelope: { attack: 0.001, decay: 0.1, release: 0.01 },
-  harmonicity: 5.1,
-  modulationIndex: 32,
-  resonance: 4000,
-  octaves: 1.5,
-}).toDestination();
-
-const snare = new Tone.NoiseSynth({
-  noise: { type: "white" },
-  envelope: { attack: 0.001, decay: 0.2, sustain: 0 },
-}).toDestination();
-
-const drumKit: DrumKit = [kick, snare, hihat];
+// const drumKit: DrumKit = [kick, snare, hihat];
 
 const kitSequence: Sequence = [
   getTrackOfNote("C", 1, "kick"),
@@ -46,13 +31,9 @@ const kitSequence: Sequence = [
 const DEFAULT_TRACK_SET: Sequence =
   CURRENT_MODE === "synth" ? cPentatonic : kitSequence;
 
-const synths =
-  CURRENT_MODE === "drum"
-    ? drumKit
-    : DEFAULT_TRACK_SET.map(() => new Tone.Synth().toDestination());
-
-console.log(drumKit);
-console.log(synths);
+// const createSynths = () => {
+//   return DEFAULT_TRACK_SET.map(() => new Tone.Synth().toDestination());
+// };
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -62,50 +43,108 @@ function App() {
 
   const beatRef = useRef(0);
   const sequenceRef = useRef(sequence);
+  const synthsRef = useRef<
+    (Tone.Synth | Tone.MembraneSynth | Tone.NoiseSynth | Tone.MetalSynth)[]
+  >([]);
+
+  // sequence.forEach((track) => console.table(track));
   Tone.getTransport().bpm.value = bpm;
 
+  const wsRef = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080"); // adjust port as needed
+    wsRef.current = ws;
+
+    ws.onopen = () => {
+      console.log("[WebSocket] Connected");
+    };
+
+    ws.onerror = (err) => {
+      console.error("[WebSocket] Error:", err);
+    };
+
+    ws.onmessage = (event) => {
+      console.log("[WebSocket] Message:", event.data);
+      console.log(event.data);
+      const sequenceData = JSON.parse(event.data);
+      updateSequence(sequenceData.trackIndex, sequenceData.stepIndex);
+      // When we receive an incoming message
+      // updateSequence with the instructions for how to change state
+    };
+
+    ws.onclose = () => {
+      console.warn("[WebSocket] Connection closed");
+      wsRef.current = null;
+    };
+
+    return () => {
+      // ws.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (CURRENT_MODE === "drum") {
+      const kick = new Tone.MembraneSynth().toDestination();
+
+      const hihat = new Tone.MetalSynth({
+        envelope: { attack: 0.001, decay: 0.1, release: 0.01 },
+        harmonicity: 5.1,
+        modulationIndex: 32,
+        resonance: 4000,
+        octaves: 1.5,
+      }).toDestination();
+
+      const snare = new Tone.NoiseSynth({
+        noise: { type: "white" },
+        envelope: { attack: 0.001, decay: 0.2, sustain: 0 },
+      }).toDestination();
+      synthsRef.current = [kick, snare, hihat];
+    } else {
+      const synths = DEFAULT_TRACK_SET.map(() =>
+        new Tone.Synth().toDestination()
+      );
+      synthsRef.current = synths;
+
+      return () => {
+        synths.forEach((synth) => synth.dispose());
+      };
+    }
+  }, []);
+
   // Handles logic for triggerign sounds on each repeat or tick of the clock
-  const repeat = (time: number) => {
+  // TODO: fix memory leak :(
+  const repeat = useCallback((time: number) => {
     setCurrentStep(beatRef.current);
 
     sequenceRef.current.forEach((track, index) => {
-      const synth = synths[index];
-      console.log(synth);
+      const synth = synthsRef.current[index];
       const note = track.steps[beatRef.current];
 
       if (note.active) {
-        // Note: Different Tone.js synths require different parameters. Especially when in drum mode, there
-        // must be logic to handle the different synths used to generate the sounds.
-
-        // DRUM MODE
         if (CURRENT_MODE === "drum") {
           if (synth instanceof Tone.NoiseSynth) {
-            // Noise synth has specific parameters
             synth.triggerAttackRelease("16n", time);
           } else {
-            // Membrane and Metal synth are more similar to the standard Synth parameters
             synth.triggerAttackRelease("C1", "16n", time);
           }
         } else {
-          // SYNTH MODE
-          synth.triggerAttackRelease(note.note, "16n", time);
+          (synth as Tone.Synth).triggerAttackRelease(note.note, "16n", time);
         }
       }
-      // if (note.active) synth.triggerAttackRelease(note.note, "16n", time);
     });
+
     beatRef.current = (beatRef.current + 1) % 16;
-  };
+  }, []);
 
   // Creates clock on mount and cleans it up on unmount to avoid memory leak
   useEffect(() => {
-    // Instantiate clock
     const id = Tone.getTransport().scheduleRepeat(repeat, "16n");
 
     return () => {
-      // Clean up
       Tone.getTransport().clear(id);
     };
-  }, []);
+  }, [repeat]);
 
   useEffect(() => {
     sequenceRef.current = sequence;
@@ -148,18 +187,27 @@ function App() {
         return step;
       });
 
-      const updatedSequence = sequence.map((prevTrack, index) =>
+      const updatedSequence = prevSequence.map((prevTrack, index) =>
         index === trackIndex ? { ...prevTrack, steps: updatedSteps } : prevTrack
       );
       return updatedSequence;
     });
   };
 
+  const handleSequenceChange = (trackIndex: number, stepIndex: number) => {
+    const updateData = {
+      trackIndex,
+      stepIndex,
+    };
+    wsRef.current?.send(JSON.stringify(updateData));
+    updateSequence(trackIndex, stepIndex);
+  };
+
   return (
     <>
       <div className="px-16 grid place-items-center gap-4 mt-30">
         <button onClick={togglePlay}>{isPlaying ? "Stop" : "Start"}</button>
-
+        {/* <button onClick={sendWSMessage}>Send a WS Message</button> */}
         <form className="flex gap-4 items-center">
           <label htmlFor="tempo" className={dotStyles.label}>
             BPM
@@ -182,7 +230,7 @@ function App() {
               {track.steps.map((step, stepIndex) => (
                 <button
                   key={stepIndex}
-                  onClick={() => updateSequence(trackIndex, stepIndex)}
+                  onClick={() => handleSequenceChange(trackIndex, stepIndex)}
                   aria-label={`Step ${step.active ? "active" : "inactive"}`}
                   className={`w-8 h-8 rounded-sm cursor-pointer ${
                     step.active && stepIndex === currentStep
