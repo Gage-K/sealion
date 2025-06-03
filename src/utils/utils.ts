f6fbe8220b0a78057fa69fb0157fc737501f1778
Gage-K <krausegage@gmail.com>
2025-06-03 10:21:46 -0400

    Correct typo in intervals

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,7 +19,6 @@ const NOTES: Octave = [
   "A",
   "Bb",
   "B",
-  "C",
 ];
 
 // INTERVALS (in semitones or st)
@@ -39,14 +38,14 @@ const NOTES: Octave = [
 
 // Each scale is an array of semitones
 const MODES: Record<ModeName, Scale> = {
-  ionian: [0, 2, 4, 5, 7, 9, 11, 12], // Major scale
+  ionian: [0, 2, 4, 5, 7, 9, 11], // Major scale
   dorian: [0, 2, 3, 5, 7, 9, 10],
-  phrygian: [0, 1, 3, 5, 7, 8, 10, 12],
-  lydian: [0, 2, 4, 6, 7, 9, 11, 12],
-  mixolydian: [0, 2, 4, 5, 7, 9, 10, 12],
-  aeolian: [0, 2, 3, 5, 7, 8, 10, 12], // Natural minor scale
-  locrian: [0, 1, 3, 5, 6, 8, 10, 12],
-  pentatonic: [0, 2, 4, 7, 9, 12],
+  phrygian: [0, 1, 3, 5, 7, 8, 10],
+  lydian: [0, 2, 4, 6, 7, 9, 11],
+  mixolydian: [0, 2, 4, 5, 7, 9, 10],
+  aeolian: [0, 2, 3, 5, 7, 8, 10], // Natural minor scale
+  locrian: [0, 1, 3, 5, 6, 8, 10],
+  pentatonic: [0, 2, 4, 7, 9],
 };
 
 // Returns a full chromatic octave starting at C
