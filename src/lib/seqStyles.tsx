f15da7fda0099bec7d38215b8fd0444ad76f57db
Gage-K <krausegage@gmail.com>
2025-06-17 20:43:01 -0400

    Add input styling

--- src/lib/seqStyles.tsx
+++ src/lib/seqStyles.tsx
@@ -6,7 +6,8 @@ export const dotStyles = {
   dotInactive: `${baseDotStyle} bg-neutral-200 border-b-neutral-100`,
   dotActive: `${baseDotStyle} bg-cyan-200 border-b-cyan-100 shadow-cyan-200/50`,
   dotActiveStart: `${baseDotStyle} bg-orange-400 border-b-orange-300 shadow-orange-400/50`,
-  input: "bg-neutral-100 px-2 py-1 rounded-sm",
+  input:
+    "px-2 py-1 rounded-sm col-start-2 col-end-5 slider shadow-black inset-shadow-sm inset-shadow-black/30",
   label: "font-medium",
   roundButton:
     "bg-neutral-200 p-4 rounded-3xl hover:bg-neutral-300 hover:cursor-pointer hover:shadow-sm active:bg-neutral-100",
